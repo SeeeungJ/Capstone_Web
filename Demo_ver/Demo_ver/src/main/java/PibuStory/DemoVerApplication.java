bb689bb8b3e20d24f2cb8d4429512d62026735fd
SeeeungJ <eft5674@naver.com>
2024-09-03 20:41:14 +0900

    master update

---
+++ Demo_ver/Demo_ver/src/main/java/PibuStory/DemoVerApplication.java
@@ -0,0 +1,14 @@
+package PibuStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DemoVerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVerApplication.class, args);
+	}
+
+}
---
+++ Demo_ver/Demo_ver/src/test/java/PibuStory/DemoVerApplicationTests.java
@@ -0,0 +1,13 @@
+package PibuStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
